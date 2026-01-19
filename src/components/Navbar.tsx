17115d5707868f1046a7daa2e15c887371a20efd
tarikceylan <tarikceylan@outlook.com.tr>
2026-01-19 17:12:23 +0300

    Update nav for home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import { useAuth } from '@/contexts/AuthContext';
 import { logoutUserAction } from '@/lib/actions';
+import Link from 'next/link';
 import { useTransition } from 'react';
 
 export const Navbar = () => {
@@ -15,17 +16,23 @@ export const Navbar = () => {
   };
 
   return (
-    <nav className='w-full'>
-      <div className='flex justify-end'>
-        {user && (
-          <div className='flex items-center justify-center gap-2'>
-            <span>{user.username}</span>
-            <button onClick={handleLogoutButtonClick} disabled={isPending}>
-              {isPending ? 'Logging out...' : 'Logout'}
-            </button>
+    <nav className='flex justify-between w-full h-10 p-2 gap-2 bg-gray-200'>
+      <div>TCBlog</div>
+      {user && (
+        <>
+          <div className='flex w-full justify-between'>
+            <div className='flex justify-center items-center'>
+              <Link href='/blogs/new'>Create New Blog</Link>
+            </div>
+            <div className='flex gap-2 justify-center items-center'>
+              <span>{user.username}</span>
+              <button onClick={handleLogoutButtonClick} disabled={isPending}>
+                {isPending ? 'Logging out...' : 'Logout'}
+              </button>
+            </div>
           </div>
-        )}
-      </div>
+        </>
+      )}
     </nav>
   );
 };
