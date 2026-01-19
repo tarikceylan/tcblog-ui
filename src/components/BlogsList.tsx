a377a75293139e6276e12225bf2f0c0b30a211b8
tarikceylan <tarikceylan@outlook.com.tr>
2026-01-19 16:39:12 +0300

    Add No blogs found

--- src/components/BlogsList.tsx
+++ src/components/BlogsList.tsx
@@ -4,6 +4,13 @@ import { BlogCard } from './BlogCard';
 export const BlogsList = async () => {
   const blogs = await getActiveBlogs();
 
+  if (!blogs.length) {
+    return (
+      <main className='flex items-center justify-center h-screen'>
+        <h1>No blogs found!</h1>
+      </main>
+    );
+  }
   return (
     <main>
       {blogs.map((blog) => {
